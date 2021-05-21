a16d2449599961a90b92e336e64ec17902c06168
Jihyeon Song <thdwl10@gmail.com>
2021-05-21 11:00:42 +0900

    영화 데이터 가져옴

---
+++ final-pjt-front/src/components/MovieList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>MovieList</h2>
+    <h1 v-for="(movie,idx) in movies" :key="idx" movie="movie" >
+      {{ movie.title }}
+    </h1>
+  </div>
+</template>
+
+<script>
+// import { mapState } from 'vuex'
+
+export default {
+  name:'MovieList',
+  props: {
+    movies: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- final-pjt-front/src/store/index.js
+++ final-pjt-front/src/store/index.js
@@ -1,15 +1,60 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import axios from 'axios'
+
 Vue.use(Vuex)
+
+const SERVER_URL = process.env.VUE_APP_SERVER_URL
 
 export default new Vuex.Store({
   state: {
+    movies: null,
+    config: null,
+    selectedMovie: null,
   },
   mutations: {
+    SET_TOKEN: function(state, config) {
+      state.config = config
+      console.log(config)
+    },
+    GET_MOVIES: function(state, movies) {
+      state.movies = movies
+      console.log(movies)
+    },
+    SELECT_MOVIE: function(state, selectedMovie) {
+      state.selectedMovie = selectedMovie
+      console.log(selectedMovie)
+    }
   },
   actions: {
+    setToken: function({commit} ) {
+      const token = localStorage.getItem('jwt')
+      const config = {
+        headers: {
+          Authorization: `JWT ${token}`
+        }
+      }
+      commit('SET_TOKEN', config)
+    },
+    getMovies: function ({commit}) {
+      const config = this.settoken()
+      axios.get(`${SERVER_URL}/movies/movie/`,config)
+      .then((res) => {
+        console.log(res)
+        commit('GET_MOVIES', res)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    },
+    selectMovie: function ({commit}, selectedMovie ) {
+      commit('SELECT_MOVIE',selectedMovie )
+    }
   },
   modules: {
+  },
+  getters: {
+
   }
 })
