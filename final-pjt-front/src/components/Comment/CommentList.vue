d4b16e3b67c10f37d73e896240a1b5812c0f90ab
Jihyeon Song <thdwl10@gmail.com>
2021-05-24 00:46:02 +0900

    댓글 수정, 삭제 기능 구현

--- final-pjt-front/src/components/Comment/CommentList.vue
+++ final-pjt-front/src/components/Comment/CommentList.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(comment, idx) in review.comment_set" v-bind:key="idx" :comment="comment">
-        {{ comment.content }}
-        <button @click="deleteComment(comment.id)">Delete</button>
-      </li>
-    </ul>
+    <CommentListItem v-for="(comment, idx) in this.review.comment_set" v-bind:key="idx" :idx="idx" :comment="comment" :review="review"/>
     <div>
       <label for="content">댓글 : </label>
       <input type="text" id="content" v-model.trim="comment.content" @keypress.enter="createComment">
@@ -16,11 +11,15 @@
 
 <script>
 import axios from 'axios'
+// import _ from 'lodash'
 const SERVER_URL = process.env.VUE_APP_SERVER_URL
+import CommentListItem from '@/components/Comment/CommentListItem'
 
 export default {
   name: 'CommentList',
-
+  components: {
+    CommentListItem,
+  },
   props: {
     review: {
       type: [Array, Object],
@@ -28,6 +27,7 @@ export default {
   },
   data: function () {
     return {
+      update: true,
       comments: this.review.comment_set,
       comment: {
         content: '',
@@ -51,7 +51,7 @@ export default {
       }
       const newComments = this.review.comment_set
       // console.log(this.comment.content)
-      axios.post(`${SERVER_URL}/community/review/${this.review.id}/comment`,commentItem, config)
+      axios.post(`${SERVER_URL}/community/review/${this.review.id}/comment/`,commentItem, config)
       .then((res) => {
         console.log(res.data)
         newComments.push(res.data)
@@ -60,26 +60,6 @@ export default {
          console.log(err)
          })
          this.comments = newComments
-    },
-    deleteComment: function (commentId) {
-      const config = this.setToken()
-      const newComments = this.review.comment_set
-      // const commentId = commentId
-      console.log(commentId)
-      axios.delete(`${SERVER_URL}/community/comment/${commentId}/`,config)
-      .then((res) => {
-          console.log(res)
-          // const targetCommentIdx = newComments.findIndex((comment) => {
-          //   return comment.id === res.data.id
-          // })
-          console.log(commentId)
-          newComments.splice(commentId,1)
-          })
-          .catch((err) => {
-          console.log(err)
-          })
-         this.comments = newComments
-
     },
   },
 }
---
+++ final-pjt-front/src/components/Comment/CommentListItem.vue
@@ -0,0 +1,88 @@
+<template>
+  <li class="d-flex justify-content-center">
+    <div v-if="update">
+      {{ comment.content }}
+      <button @click="deleteComment(comment,idx)">Delete</button>
+    </div>
+    <div v-else >
+      <input type="text" v-model.trim="comment.content">
+    </div>
+    <div v-if="update">
+      <button @click="updateComment">Update</button>
+    </div>
+    <div v-else>
+      <button @click="updateComplete(comment)">Update완료</button>
+    </div>
+  </li>
+</template>
+
+<script>
+import axios from 'axios'
+// import _ from 'lodash'
+const SERVER_URL = process.env.VUE_APP_SERVER_URL
+
+export default {
+  name:'CommentListItem',
+  props: {
+    comment: {
+      type: [Array, Object],
+    },
+    idx: Number,
+    review: {
+      type: [Array, Object]
+    }
+  },
+  data: function () {
+    return {
+      update: true,
+    }
+  },
+  methods: {
+    setToken: function( ) {
+      const token = localStorage.getItem('jwt')
+      const config = {
+        headers: {
+          Authorization: `JWT ${token}`
+        }
+      }
+      return config
+    },
+    deleteComment: function (comment,idx) {
+      const config = this.setToken()
+      const newComments = this.review.comment_set
+      const commentIdx = idx
+      axios.delete(`${SERVER_URL}/community/comment/${comment.id}/`,config)
+      .then((res) => {
+          console.log(res)
+          newComments.splice(commentIdx,1)
+          })
+          .catch((err) => {
+          console.log(err)
+          })
+         this.comments = newComments
+
+    },
+    updateComment: function () {
+      this.update = false
+    },
+    updateComplete: function (comment) {
+      const config = this.setToken()
+      const commentItem = {
+        'content': this.comment.content,
+      }
+      axios.put(`${SERVER_URL}/community/comment/${comment.id}/`,commentItem, config)
+      .then((res) => {
+          console.log(res)
+          this.update = true
+          })
+          .catch((err) => {
+          console.log(err)
+          })
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
