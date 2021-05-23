5ee8afde71f989c9f782875a1e4039327305f7ef
sj <asj12896@gmail.com>
2021-05-23 14:00:25 +0900

    회원가입 수정

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -8,3 +8,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'followings', 'followers', 'id',)
+        read_only_fields = ('followings', 'followers',)
