8a8e634c9c3b3a79de69aa6d819a82ae97ae1c78
Jihyeon Song <thdwl10@gmail.com>
2021-05-26 23:46:03 +0900

    overview 수정

--- final-pjt-front/src/components/Movie/MovieDetail.vue
+++ final-pjt-front/src/components/Movie/MovieDetail.vue
@@ -2,33 +2,35 @@
 <div id="movieDetailBox">
   <div class="card">
     <!-- {{ movieDetail }} -->
-    <div class="card_left">
+    <div class="card-up">
+      <div class="card_left">
         <img class="" :src="movieImage" alt="movie_image"> 
-    </div>
-    <div id="contentBox" class="card_right">
-      <h1>{{ movieDetail.title }}</h1>
-      <div class="card_right_details">
-        <ul id="card_right_details_ul">
-          <li>개봉일 : {{ movieDetail.release_date }}</li>
-          <li>장르 : </li>
-        </ul>
-        <div id="card_right_rating">
-          <div class="card_right_rating_stars">
-            <fieldset id="ration" class='rating'>
-              <input id='star10' name='rating' type='radio' value='10'>
-              <label class='full' for='star10' title='10 stars'></label>
-              <input id='star9half' name='rating' type='radio' value='9 and a half'>
-              <label class='half' for='star9half' title='9.5 stars'></label>
-           </fieldset>
+      </div>
+      <div id="contentBox" class="card_right">
+        <h1>{{ movieDetail.title }}</h1>
+        <div class="card_right_details">
+          <ul id="card_right_details_ul">
+            <li>개봉일 : {{ movieDetail.release_date }}</li>
+            <li>장르 : </li>
+          </ul>
+          <div id="card_right_rating">
+            <div class="card_right_rating_stars">
+              <fieldset id="ration" class='rating'>
+                <input id='star10' name='rating' type='radio' value='10'>
+                <label class='full' for='star10' title='10 stars'></label>
+                <input id='star9half' name='rating' type='radio' value='9 and a half'>
+                <label class='half' for='star9half' title='9.5 stars'></label>
+            </fieldset>
+            </div>
           </div>
-        </div>
-        <div id="card_right_overview">
-          <p>{{ movieDetail.overview}}</p>
+          <div id="card_right_overview">
+            <p>{{ overview }}</p>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <div>
+  <div id="card_down">
     <MovieReviews :movieId="movieId"/>
     <button @click="goCreateReview" >리뷰쓰기</button>
   </div>
@@ -36,17 +38,10 @@
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 import {mapState} from 'vuex'
 import MovieReviews from '@/components/Movie/MovieReviews'
 
-// const api = axios.create({
-//   baseUrl:'https://api.themoviedb.org/3/movie/',
-//   params: { 
-//     apikey: "3dbbe66dede6e1bfdc61c7773b51e2ee",
-//     language: "ko-KR"
-//   }
-// })
 
 export default {
   name: 'MovieDetail',
@@ -56,28 +51,22 @@ export default {
   data: function () {
     return {
       movieId: this.$route.params.movieId,
+      overview: '',
     }
   },
    methods: {
     goCreateReview: function() {
       this.$router.push({ name: 'CreateReview'})
     },
-    getMovieImages: function() {
-      axios({
-        method: 'get',
-        url: `https://api.themoviedb.org/3/movie/${this.movieId}/images`,
-        params: {
-          api_key: "3dbbe66dede6e1bfdc61c7773b51e2ee",
-          language: "ko-KR",
-        }
-      })
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    },
+    overviewSplit: function () {
+      if (this.movieDetail.overview.length > 150) {
+        this.overview = this.movieDetail.overview.substring(0,150)+'....'
+      }else {
+        this.overview = this.movieDetail.overview
+      }
+      console.log('오버뷰')
+      console.log(this.overview)
+    }
   },
   computed: {
     movieImage: function () {
@@ -85,15 +74,15 @@ export default {
     },
     ...mapState([
       'movieDetail',
-    ])
+    ]),
   },
   mounted() {
-    this.getMovieImages()
+    this.overviewSplit()
   }
 }
 </script>
 
-<style>
+<style scoped>
 @import url(https://fonts.googleapis.com/css?family=Montserrat:400,700);
 @import url(//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
 
@@ -104,11 +93,11 @@ export default {
   margin-top: 60px;
 } */
 .card {
-  width: 800px;
-  height: 450px;
+  width: 600px;
+  height: 350px;
   background: transparent;
   position: absolute;
-  display: inline-block;
+  /* display: inline-block; */
   left: 0;
   right: 0;
   margin: auto;
@@ -117,20 +106,26 @@ export default {
   border-radius: 10px;
   -webkit-border-radius: 10px;
   -moz-border-radius: 10px;
+
+}
+#card_doun{
+  display: inline-block;
+  width: 600px;
+  border:1px solid rgba(0,0,0);
   box-shadow: 0px 20px 30px 3px rgba(0, 0, 0, 0.55);
 }
 .card_left {
   width: 40%;
-  height: 450px;
+  height: 350px;
   float: left;
   overflow: hidden;
   background: transparent;
 }
 .card_left img {
   width: 100%;
-  height: auto;
+  height: 350px;
   border-radius: 10px 0 0 10px;
-  -webkit-border-radius: 10px 0 0 10px;
+  -webkit-border-radius: 10px 0 0 0px;
   -moz-border-radius: 10px 0 0 10px;
   position: relative;
 }
@@ -139,18 +134,18 @@ export default {
   width: 60%;
   float: left;
   background: #000000;
-  height: 450px;
+  height: 350px;
   border-radius: 0 10px 10px 0;
-  -webkit-border-radius: 0 10px 10px 0;
+  -webkit-border-radius: 0 10px 0px 0;
   -moz-border-radius: 0 10px 10px 0;
 }
 .card_right h1 {
   color: white;
   font-family: "Montserrat", sans-serif;
   font-weight: 400;
-  text-align: left;
-  font-size: 40px;
-  margin: 40px 0 40px 0;
+  /* text-align: left; */
+  font-size: 20px;
+  margin: 30px 0 30px 0;
   padding: 0 0 0 0;
   text-align: center;
   letter-spacing: 1px;
@@ -230,10 +225,10 @@ export default {
 #card_right_overview p{
   color: white;
   font-family: "Montserrat", sans-serif;
-  font-size: 15px;
+  font-size: 12px;
   padding: 0 40px 0 40px;
   letter-spacing: 1px;
-  margin: 40px 0 10px 0;
+  margin: 20px 0 10px 0;
   line-height: 20px;
 }
 
@@ -297,11 +292,9 @@ export default {
   margin-left: 0px;
   margin-right: 0px;
 }
-
 #imageContainer {
   width: 100%;
   height: 300px;
   background-color: black;
 } */
-
 </style>
